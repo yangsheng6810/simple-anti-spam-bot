048bedd918496009493f9a0ed32046a086f9b7e5
Sheng Yang <styang@fastmail.com>
2022-02-24 18:50:08 -0600

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,53 @@
-use log;
-use teloxide::Bot;
-use teloxide::types::Message;
-use teloxide::prelude::*;
-use anyhow::Result;
+// This bot answers how many messages it received in total on every message.
 
-struct MyBotHandler {}
- 
-impl<R> teloxide::dispatching::DispatcherHandler<R, Message> for MyBotHandler
-where
-    R: Send + 'static,
-{
-    fn handle(
-        self,
-        rx: DispatcherHandlerRx<R, Message>,
-    ) -> futures_util::future::BoxFuture<'static, ()>
-    where
-        UpdateWithCx<R, Message>: Send + 'static,
-    {
-        return futures_util::FutureExt::boxed(bot_handle_message(rx));
-    }
-}
- 
-async fn bot_handle_message<R>(rx: DispatcherHandlerRx<R, Message>)
-where
-    R: Send + 'static,
-{
-    tokio_stream::wrappers::UnboundedReceiverStream::new(rx)
-        .for_each_concurrent(None, |message| async move {
-            dbg!(message.update);
-        })
-        .await;
-}
+use std::sync::atomic::{AtomicU64, Ordering};
+
+use once_cell::sync::Lazy;
+use teloxide::prelude2::*;
+
+
+static MESSAGES_TOTAL: Lazy<AtomicU64> = Lazy::new(AtomicU64::default);
 
 #[tokio::main]
-pub async fn main() -> Result<()> {
-    log::info!("Starting bot thread...");
-    let bot = Bot::new("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-        .parse_mode(teloxide::types::ParseMode::MarkdownV2);
- 
-    let message_handler = MyBotHandler {};
-    Dispatcher::new(bot)
-        .messages_handler(message_handler)
-        .dispatch()
-        .await;
-    return Ok(());
+async fn main() {
+    teloxide::enable_logging!();
+    log::info!("Starting shared_state_bot...");
+
+    let bot = Bot::from_env().auto_send();
+
+    let handler = Update::filter_message()
+        .branch(dptree::endpoint(
+        |msg: Message, bot: AutoSend<Bot>| async move {
+            let previous = MESSAGES_TOTAL.fetch_add(1, Ordering::Relaxed);
+            bot.send_message(msg.chat.id, format!("I received {} messages in total.", previous))
+                .await?;
+            respond(())
+        },
+    ))
+        // .branch(
+        //     // Filter a maintainer by a used ID.
+        //     dptree::filter(|msg: EditedMessage, cfg: ConfigParameters| {
+        //         msg.from().map(|user| user.id == cfg.bot_maintainer).unwrap_or_default()
+        //     })
+        //     .filter_command::<MaintainerCommands>()
+        //     .endpoint(
+        //         |msg: Message, bot: AutoSend<Bot>, cmd: MaintainerCommands| async move {
+        //             match cmd {
+        //                 MaintainerCommands::Rand { from, to } => {
+        //                     let mut rng = rand::rngs::OsRng::default();
+        //                     let value: u64 = rng.gen_range(from..=to);
+
+        //                     bot.send_message(msg.chat.id, value.to_string()).await?;
+        //                     Ok(())
+        //                 }
+        //             }
+        //         },
+        //     ),
+        // )
+        ;
+
+    Dispatcher::builder(bot, handler)
+        .build()
+        .setup_ctrlc_handler()
+        .dispatch().await;
 }
